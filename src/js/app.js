7cef8ffec496136c9ef932ddec251e55184ebdd3
Prakash CK <108185015+PrakashChinnaduraiK@users.noreply.github.com>
2023-01-31 05:16:25 +0530

    have added app.js

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+import {secretButton, secretParagraph} from './dom-loader';
+import '../css/main.css';
+import '../css/input-elements.css';
+var showSecret = false;
+
+secretButton.addEventListener('click', toggleSecretState);
+updateSecretParagraph();
+
+function toggleSecretState() {
+    showSecret = !showSecret;
+    updateSecretParagraph();
+    updateSecretButton()
+}
+
+function updateSecretButton() {
+    if (showSecret) {
+        secretButton.textContent = 'Hide the High Priority bugs list';
+    } else {
+        secretButton.textContent = 'Show the High Priority bugs list';
+    }
+}
+
+function updateSecretParagraph() {
+    if (showSecret) {
+        secretParagraph.style.display = 'block';
+    } else {
+        secretParagraph.style.display = 'none';
+    }
+}
